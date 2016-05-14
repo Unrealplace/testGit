d84f2c4e89013b9d6053926dc5f222f819d71859
Unrealplace <liyang84681111819@qq.com>
2016-05-14 08:59:44 +0800

    Initial Commit

---
+++ testGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by LiYang on 16/5/14.
+//  Copyright © 2016年 LiYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by LiYang on 16/5/14.
+//  Copyright © 2016年 LiYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
